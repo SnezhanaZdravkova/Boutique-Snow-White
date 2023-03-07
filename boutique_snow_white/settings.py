834bcb580e5d4efa2145d3082b0b9644d8902502
Snezhana Zdravkova <snezhanazdravkova7@gmail.com>
2023-03-07 15:38:41 +0000

    completed home page header and css.

--- boutique_snow_white/settings.py
+++ boutique_snow_white/settings.py
@@ -144,6 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
+MEDIA_URL = '/media/'
+MEDIA_RUTH = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

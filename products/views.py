5732e043dd6f01036adbcdb9477095785254206b
Snezhana Zdravkova <snezhanazdravkova7@gmail.com>
2023-03-24 10:42:02 +0000

    Add import Lower

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 
@@ -37,7 +38,7 @@ def all_products(request):
             queries = Q(
                        name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
-    
+
     current_sorting = f'{sort}_{direction}'
 
     context = {

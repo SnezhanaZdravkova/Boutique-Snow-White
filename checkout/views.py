ccf386c9d6aa9008a368f5c36affd0642a1631e1
Snezhana Zdravkova <snezhanazdravkova7@gmail.com>
2023-03-31 10:19:49 +0000

    Add stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51MrN6jBKEiVUC6eNIZxvK5h0AyCcrk0nauJcMakcibgaR5Teoi3mDFzj7I5jd70mIBKoEQTCQmpCBhafo9zTNfCv00fjVxdEWu',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)

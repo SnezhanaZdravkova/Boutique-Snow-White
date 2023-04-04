36100beb1bb2d5460e5b3fa0036b58ccb6c2dbae
Snezhana Zdravkova <snezhanazdravkova7@gmail.com>
2023-04-04 16:17:28 +0000

    Add profile form, view and update templates

--- profiles/views.py
+++ profiles/views.py
@@ -6,9 +6,25 @@ def profile(request):
     """ Display the user's profile. """
 
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request,
+                           ('Update failed. Please ensure '
+                            'the form is valid.'))
+    else:
+        form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
+
     template = 'profiles/profile.html'
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)

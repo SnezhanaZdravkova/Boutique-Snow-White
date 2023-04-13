631a3c3b297e84462b66b25a750fe704c6795f1f
SnezhanaZdravkova <>
2023-04-13 16:29:46 +0000

    Add view to contact app

--- contact/urls.py
+++ contact/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ContactView.as_view(), name='contact'),
+    path('contact', views.contact, name='contact'),
+    # path('', views.ContactView.as_view(), name='contact'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,67 +1,41 @@
-from django.shortcuts import render
-from django.contrib import messages
+from django.shortcuts import render, redirect, reverse
 from django.core.mail import send_mail
-from django.views import generic, View
+from django.contrib import messages
 from django.conf import settings
-from django.template.loader import render_to_string
 
 from .forms import ContactForm
-from .models import Contact
 
 
-class ContactView(View):
+def contact(request):
     """
-    This view is used to display the contact form and
-    handle GET and POST requests
+    Submits contact us form to the admin dashboard.
     """
+    submitted = False
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name'],
+            email = form.cleaned_data['email'],
+            subject = form.cleaned_data['subject'],
+            message = form.cleaned_data['message'],
+            form.save()
 
-    def get(self, request):
-        """
-        Renders the contact form
-        """
-        if request.user.is_authenticated:
-            form = ContactForm(initial={'email': request.user.email})
+            send_mail({subject}, f'{name}, {email}, {message}',
+                      settings.EMAIL_HOST_USER, [settings.EMAIL_HOST_USER],
+                      fail_silently=False)
+            messages.success(request,
+                             f'Thank you for Contacting Us.'
+                             f'we will reply within 24 hrs!'
+                             )
+
+            return redirect(reverse('home'))
         else:
-            form = ContactForm()
+            messages.error(request,
+                           f'Something went wrong with your Submission.'
+                           f'Please try again.'
+                           )
 
-        return render(
-            request,
-            "contact/contact.html",
-            {
-                "contact_form": form,
-            },
-        )
+    else:
+        form = ContactForm()
 
-    def post(self, request):
-
-        contact_form = ContactForm(data=request.POST)
-        if contact_form.is_valid():
-            cust_name = contact_form.instance.name
-            cust_email = contact_form.instance.email
-            subject = contact_form.instance.subject
-
-            message = contact_form.instance.message
-            message = render_to_string(
-                'contact/confirmation_emails/confirmation_email_body.txt',
-                {'cust_name': cust_name, 'message': message}
-            )
-            send_mail(
-                subject,
-                message,
-                settings.DEFAULT_FROM_EMAIL,
-                [cust_email]
-            )
-            contact_form.save()
-            messages.success(self.request, 'Your enquiry has been sent')
-
-            target = "home/index.html"
-            context = {"plain_message": True}
-        else:
-            messages.error(request, """Form failed. Please ensure the
-            form is valid """)
-            target = "contact/contact.html"
-            context = {
-                "plain_message": True,
-                "contact_form": contact_form
-                }
-        return render(request, target, context)
+    return render(request, 'contact/contact.html', {'form': form})

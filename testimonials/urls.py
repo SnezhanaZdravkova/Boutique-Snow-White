2d987a708caafe45c3303cbd0725ce5e6ac043df
SnezhanaZdravkova <>
2023-05-12 13:24:10 +0000

    Add add_testimonial

--- testimonials/urls.py
+++ testimonials/urls.py
@@ -4,4 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.Services.as_view(), name='services'),
     path('testimonials/', views.Testimonials.as_view(), name='testimonials'),
+    path(
+        'testimonials/add/', views.AddTestimonial.as_view(),
+        name='add_testimonial'
+        ),
 ]
--- testimonials/views.py
+++ testimonials/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, UpdateView, ListView
+from django.views.generic import TemplateView, UpdateView, ListView, CreateView
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Service, Testimonial
+from .forms import ServiceForm, TestimonialForm
+
 
 class Services(ListView):
     """ This view is used to display all services """
@@ -25,3 +27,21 @@ class Testimonials(ListView):
         context = super().get_context_data(**kwargs)
         context['plain_message'] = True
         return context
+
+
+class AddTestimonial(
+        LoginRequiredMixin, UserPassesTestMixin,
+        SuccessMessageMixin, CreateView):
+
+    """This view is used to allow a user to add a testimonial"""
+    form = TestimonialForm
+    template_name = 'testimonials/add_testimonial.html'
+    success_message = "Your testimonial was added successfully"
+
+    def form_valid(self, form):
+        """
+        Override the form_valid() method in model form view
+        in order to set the signed in user as the name on the testimonial.
+        """
+        form.instance.name = self.request.user
+        return super().form_valid(form)

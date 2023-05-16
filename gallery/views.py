870b562a0bceac7c43e8421c6a64c69824e559a8
SnezhanaZdravkova <>
2023-05-16 15:22:43 +0000

    Add success_url and everse_lazy imported

--- gallery/views.py
+++ gallery/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import DeleteView, ListView, CreateView
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 from .models import GalleryImages
 from .forms import AddImageForm
 
@@ -24,6 +25,7 @@ class AddImage(
     form_class = AddImageForm
     template_name = 'gallery/add_image.html'
     success_message = "Your image was added successfully"
+    success_url = reverse_lazy('services')
 
     def test_func(self):
         """

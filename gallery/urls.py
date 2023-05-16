3225fac79725b66cd0a827c4db4d2da73d3a5e5d
SnezhanaZdravkova <>
2023-05-16 11:17:35 +0000

    Fixed line too long

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,6 +3,7 @@ from .views import ProjectImages, AddImage
 
 
 urlpatterns = [
-    path('gallery/', ProjectImages.as_view(), name='project_images'),
+    path('gallery/',
+         ProjectImages.as_view(), name='project_images'),
     path('add_image/', AddImage.as_view(), name='add_image'),
 ]

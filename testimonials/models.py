45b930bd8e8b0349ce76f7fd2ef4a13d3af982be
SnezhanaZdravkova <>
2023-06-13 11:33:32 +0000

    Removed @receiver def create_user_profile, as I don't need it

--- testimonials/models.py
+++ testimonials/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 
 
 class Service(models.Model):
@@ -24,7 +22,8 @@ class Testimonial(models.Model):
     name = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="testimonials")
     service = models.ForeignKey(
-        Service, on_delete=models.CASCADE, related_name='testimonial', null=True, blank=True)
+        Service, on_delete=models.CASCADE, related_name='testimonial',
+        null=True, blank=True)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now=True)
 
@@ -37,10 +36,3 @@ class Testimonial(models.Model):
     def get_absolute_url(self):
         """Get url after user adds/edits testimonial"""
         return reverse('testimonials')
-
-
-@receiver(post_save, sender=User)
-def create_user_profile(instance, created, **kwargs):
-    """Create or update the user profile"""
-    if created:
-        Testimonial.objects.create(name=instance)
